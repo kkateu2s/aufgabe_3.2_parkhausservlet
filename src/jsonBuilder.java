bb663916066daf6a7c652c78326d9a2bf345b36a
MrBoneyPantsGuy <64370314+MrBoneyPantsGuy@users.noreply.github.com>
2020-06-16 14:05:20 +0200

    Added Javax.json with example

--- src/jsonBuilder.java
+++ src/jsonBuilder.java
@@ -1,7 +1,26 @@
 import javax.json.*;
+import javax.json.stream.JsonParser;
+import java.io.StringReader;
 
 public class jsonBuilder {
+
+    public int addAllValues(JsonObject object) {
+        int sum = 0;
+
+        JsonParser parser = Json.createParser(new StringReader(object.toString()));
+        while(parser.hasNext()) {
+            JsonParser.Event event = parser.next();
+            switch (event) {
+                case VALUE_NUMBER:
+                    sum += Integer.valueOf(parser.getString());
+                    break;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
+        // First TestObject
         JsonObject personObject = Json.createObjectBuilder()
                 .add("name", "John")
                 .add("age", 13)
@@ -19,7 +38,61 @@ public class jsonBuilder {
                                 .build()
                 )
                 .build();
+        System.out.println("Object: " + personObject); // Looking good
 
-        System.out.println("Object: " + personObject);
+        // Second TestObject
+        JsonObject NumbersToAdd = Json.createObjectBuilder()
+                .add("Value1" , 5)
+                .add("Value2", 7)
+                .add("moreValues",
+                        Json.createObjectBuilder()
+                                .add("moreValues1", 10)
+                                .add("moreValues2", 2)
+                                .add("alotmoreValues",
+                                        Json.createObjectBuilder()
+                                                .add("alotmoreValues1", 3)
+                                                .add("alotmoreValues2", 4)
+                                                .add("anArrayOfValues", Json.createArrayBuilder()
+                                                        .add(5)
+                                                        .add(6)
+                                                        .add(7)
+                                                        .build()
+                                                )
+                                                .build()
+                                )
+                                .build()
+                )
+                .build();
+        System.out.println("MyObject: " + NumbersToAdd); // Looking good
+
+        // Third TestObject
+        JsonObject NumbersToAddEasy = Json.createObjectBuilder()
+                .add("Value1" , 2)
+                .add("Value2", 2)
+                .add("moreValues",
+                        Json.createObjectBuilder()
+                                .add("moreValues1", 2)
+                                .add("moreValues2", 2)
+                                .add("alotmoreValues",
+                                        Json.createObjectBuilder()
+                                                .add("alotmoreValues1", 2)
+                                                .add("alotmoreValues2", 2)
+                                                .add("anArrayOfValues", Json.createArrayBuilder()
+                                                        .add(2)
+                                                        .add(2)
+                                                        .add(2)
+                                                        .add("Randomtext")
+                                                        .add(2)
+                                                        .build()
+                                                )
+                                                .build()
+                                )
+                                .build()
+                )
+                .build();
+        System.out.println("MyObject: " + NumbersToAddEasy); // Looking good
+
+        jsonBuilder builder = new jsonBuilder();
+        System.out.println(builder.addAllValues(NumbersToAddEasy));
     }
 }
